9cf49f10548fea0ddb92f7ba862121ffcffb68ea
dawblo <dawid.bloch96@gmail.com>
2020-03-04 23:18:17 +0100

    Dodałem ja

--- JB/src/com/jbstart/funkcje/Daw.java
+++ JB/src/com/jbstart/funkcje/Daw.java
@@ -1,4 +1,14 @@
 package com.jbstart.funkcje;
 
 public class Daw {
+    String wlasciciel;
+    public Daw(String wlasciciel){
+        this.wlasciciel = wlasciciel;
+        Wlasciciel();
+    }
+
+    private void Wlasciciel(){
+        System.out.println("Jestem "+this.wlasciciel+", właścicielem konia ");
+    }
+
 }
--- JB/src/com/jbstart/main.java
+++ JB/src/com/jbstart/main.java
@@ -1,9 +1,12 @@
 package com.jbstart;
 import com.jbstart.funkcje.Rad;
+import com.jbstart.funkcje.Daw;
 
 public class main {
     public static void main(String[] args)
+
     {
         Rad kon = new Rad("Koń","Agresywny",54);
+        Daw w = new Daw("JA");
     }
 }
