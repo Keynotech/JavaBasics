cf92de1380c009c35a848538b72b12e7e45b1d86
rczerwiec <czerwiecr1999@gmail.com>
2020-03-04 22:48:23 +0100

    Rad - klasa ktora sluzy do stworzenia obiektu zwierzecia
    metody:
    konstruktor pobierajacy imie,zachowanie,wiek
    Przedstaw_Sie - przedstawia sie, z pomoca pól zainicjowanych w konstruktorze.

--- JB/src/com/jbstart/funkcje/Rad.java
+++ JB/src/com/jbstart/funkcje/Rad.java
@@ -1,4 +1,21 @@
 package com.jbstart.funkcje;
 
 public class Rad {
+
+    String imie;
+    String zachowanie;
+    int wiek;
+    public Rad(String imie, String zachowanie, int wiek){
+        this.imie = imie;
+        this.zachowanie = zachowanie;
+        this.wiek = wiek;
+        Przedstaw_Sie();
+    }
+
+    private void Przedstaw_Sie(){
+        System.out.println("Jestem "+this.imie+", jestem bardzo "+this.zachowanie+" i mam "+this.wiek+" lat");
+    }
+
+
+
 }
--- JB/src/com/jbstart/main.java
+++ JB/src/com/jbstart/main.java
@@ -1,8 +1,9 @@
 package com.jbstart;
+import com.jbstart.funkcje.Rad;
 
 public class main {
     public static void main(String[] args)
     {
-        System.out.println("Start");
+        Rad kon = new Rad("Koń","Agresywny",54);
     }
 }
